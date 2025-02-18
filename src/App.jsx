5347d6a62542eb977f01e1aa1295357f550ceae2
gaetanjoannic <gaejoannic@hotmail.fr>
2025-02-18 14:48:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react'
+
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View
+} from 'react-native'
+
+function App () {
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentInsetAdjustmentBehavior='automatic'
+      >
+        <View>
+          <Text>rel:cfd</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
